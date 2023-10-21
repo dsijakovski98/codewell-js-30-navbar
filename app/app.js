e5fa87f767b35bbf3a728026d999c1da2e5a6a21
Daniel Shijakovski <daniel.shijakovski@testdevlab.com>
2023-10-22 00:48:24 +0200

    backdrop controlls added

--- app/app.js
+++ app/app.js
@@ -1,7 +1,8 @@
 const burger = document.querySelector('.burger')
 const navMenu = document.querySelector('.links-container')
+const backdrop = document.querySelector('nav .backdrop')
 
-burger.addEventListener('click', () => {
+const toggleMenu = () => {
 	const expanded = navMenu.getAttribute('aria-expanded')
 	const newExpanded = expanded === 'true' ? 'false' : 'true'
 
@@ -11,4 +12,7 @@ burger.addEventListener('click', () => {
 	const [oldIcon, newIcon] = expanded === 'true' ? ['times', 'bars'] : ['bars', 'times']
 
 	icon.className = icon.className.replace(oldIcon, newIcon)
-})
+}
+
+burger.addEventListener('click', toggleMenu)
+backdrop.addEventListener('click', toggleMenu)
