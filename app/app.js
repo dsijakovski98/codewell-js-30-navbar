155e939f0fec31c6a0d8fee5a614feabba5dc1aa
Daniel Shijakovski <daniel.shijakovski@testdevlab.com>
2023-10-22 00:44:31 +0200

    navbar js controls added

--- app/app.js
+++ app/app.js
@@ -1,1 +1,14 @@
-// TODO: Add navbar controls
+const burger = document.querySelector('.burger')
+const navMenu = document.querySelector('.links-container')
+
+burger.addEventListener('click', () => {
+	const expanded = navMenu.getAttribute('aria-expanded')
+	const newExpanded = expanded === 'true' ? 'false' : 'true'
+
+	navMenu.setAttribute('aria-expanded', newExpanded)
+
+	const icon = burger.querySelector('i')
+	const [oldIcon, newIcon] = expanded === 'true' ? ['times', 'bars'] : ['bars', 'times']
+
+	icon.className = icon.className.replace(oldIcon, newIcon)
+})
